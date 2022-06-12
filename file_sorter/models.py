45ddbef024e8b7c6c9df10b4278d4245efe354db
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-06-12 07:55:44 +0300

    dev_alexkan

--- file_sorter/models.py
+++ file_sorter/models.py
@@ -1,29 +1,14 @@
+from distutils.command.upload import upload
 from django.contrib.auth.models import User
 from django.conf import settings
 from django.db import models
 from pathlib import Path
 
-def upload_to(instance, filename):
-    CATEGORIES = {
-        'images': ('JPEG', 'PNG', 'JPG', 'SVG'),
-        'documents': ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
-        'audio': ('MP3', 'OGG', 'WAV', 'AMR'),
-        'video': ('AVI', 'MP4', 'MOV', 'MKV'),
-        'archives': ('ZIP', 'GZ', 'TAR')
-    }
-    user_subdir = instance.user.username
-    p = Path(settings.MEDIA_ROOT, user_subdir)
-    ext = Path(filename).suffix[1:]
-    for key, value in CATEGORIES.items():
-        if ext.upper() in value:
-            p = p.joinpath(key)
-            break
-    return p.joinpath(filename)
 
 class UploadFile(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='files')
     # f_name = models.CharField(max_length=255)
-    loaded_file = models.FileField(upload_to=upload_to)
+    loaded_file = models.FileField(blank=False, max_length=500) # upload_to=upload_to,
 
     def __str__(self):
         return self.loaded_file
--- file_sorter/views.py
+++ file_sorter/views.py
@@ -1,24 +1,49 @@
+from pathlib import Path
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, HttpResponse
 from file_sorter.models import UploadFile
 from .forms import UploadFileForm
 from django.contrib import messages
 from os import path
+from django.conf import settings
+import shutil
+
+def sort_files(files_list, username):
+    CATEGORIES = {
+        'images': ('JPEG', 'PNG', 'JPG', 'SVG'),
+        'documents': ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
+        'audio': ('MP3', 'OGG', 'WAV', 'AMR'),
+        'video': ('AVI', 'MP4', 'MOV', 'MKV'),
+        'archives': ('ZIP', 'GZ', 'TAR')
+    }
+    media_path = Path(settings.MEDIA_ROOT, username)
+    for file1 in files_list:
+        ext = Path(file1.name).suffix[1:]
+        for key, value in CATEGORIES.items():
+            if ext.upper() in value:
+                new_path = media_path.joinpath(key)
+                break
+        new_path.mkdir(parents=True, exist_ok=True)
+        with open(new_path.joinpath(file1.name), 'wb+') as f:
+            shutil.copyfileobj(file1, f)
+
 
 @login_required
 def uploadfile(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         files = request.FILES.getlist('loaded_file')
+        print('--- TYPE:', type(request.FILES['loaded_file']))
         if form.is_valid():
-            form = form.save(commit=False)
-            form.user = request.user
-            form.save()
-            # for f in files:
-            #     file_instance = UploadFile(user_id=str(request.user.id), loaded_file=path.join(str(request.user.id), '2', str(f)))
-            #     file_instance.save()
-            messages.success(request, 'The file is saved successfully.')
-            return redirect('file_sorter')
+            # form = form.save(commit=False)
+            # form.user = request.user
+            # form.save()
+            # sort_files(files)
+            # messages.success(request, 'The file is saved successfully.')
+            # return redirect('file_sorter')
+            sort_files(files, request.user.username)
+            context = {'msg' : '<span style="color: green;">File successfully uploaded</span>'}
+            return render(request, "uploadfoles.html", {'form': form})
         else:
             messages.error(request, f'The following error has occured: {form.errors.as_data()}')
             return redirect('file_sorter')
