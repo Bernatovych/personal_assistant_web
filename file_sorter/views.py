a724a8d377e2bb643915b26cfd898bf5fc48e6af
korshunov-olexiy <korshunov.olexiy@gmail.com>
2022-06-13 14:08:46 +0300

    save multiple files

--- file_sorter/views.py
+++ file_sorter/views.py
@@ -1,48 +1,33 @@
-from pathlib import Path
-from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect, HttpResponse
-from file_sorter.models import UploadFile
-from .forms import UploadFileForm
-from django.contrib import messages
-from os import path
-from django.conf import settings
 import shutil
+from pathlib import Path
+from django.conf import settings
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import redirect, render
+from .forms import UploadFileForm
 
-def sort_files(files_list, username):
-    CATEGORIES = {
-        'images': ('JPEG', 'PNG', 'JPG', 'SVG'),
-        'documents': ('DOC', 'DOCX', 'TXT', 'PDF', 'XLSX', 'PPTX'),
-        'audio': ('MP3', 'OGG', 'WAV', 'AMR'),
-        'video': ('AVI', 'MP4', 'MOV', 'MKV'),
-        'archives': ('ZIP', 'GZ', 'TAR')
-    }
-    media_path = Path(settings.MEDIA_ROOT, username)
+def determine_path(ext, username):
+    uploads_path = Path(settings.UPLOADS_ROOT, username)
+    cat_key, cat_val = settings.CATEGORIES.keys(), settings.CATEGORIES.values()
+    append_path = ''.join(list(map(lambda key, value: key if ext.upper() in value else '', cat_key, cat_val)))
+    append_path = append_path if len(append_path) else 'other'
+    return uploads_path.joinpath(append_path)
+
+def sort_files(req, files_list, username):
     for file1 in files_list:
-        ext = Path(file1.name).suffix[1:]
-        for key, value in CATEGORIES.items():
-            if ext.upper() in value:
-                new_path = media_path.joinpath(key)
-                break
+        new_path = determine_path(Path(file1.name).suffix[1:], username)
         new_path.mkdir(parents=True, exist_ok=True)
         with open(new_path.joinpath(file1.name), 'wb+') as f:
             shutil.copyfileobj(file1, f)
-
+    messages.success(req, message=f"files \"{', '.join([f.name for f in files_list])}\" successfully uploaded")
 
 @login_required
 def uploadfile(request):
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
         files = request.FILES.getlist('loaded_file')
-        print('--- TYPE:', type(request.FILES['loaded_file']))
         if form.is_valid():
-            # form = form.save(commit=False)
-            # form.user = request.user
-            # form.save()
-            # sort_files(files)
-            # messages.success(request, 'The file is saved successfully.')
-            # return redirect('file_sorter')
-            sort_files(files, request.user.username)
-            context = {'msg' : '<span style="color: green;">File successfully uploaded</span>'}
+            sort_files(request, files, request.user.username)
             return render(request, "uploadfoles.html", {'form': form})
         else:
             messages.error(request, f'The following error has occured: {form.errors.as_data()}')
